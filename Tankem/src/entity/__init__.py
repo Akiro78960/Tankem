69f8e72432a9614b4711796c63a94f6b1b0376e2
Marc-Andre Lavoie <phoenix.lavoie@gmail.com>
2016-01-22 09:26:06 -0500

    ********************VERSION QUI NE COMPILE PAS************************* -Structure: Création de modules pour mieux structurer le code. À cause d'une limitation de GIT, on doit changer le code en deuxième temps.

---
+++ Tankem/src/entity/__init__.py
@@ -0,0 +1,3 @@
+from . import item
+from . import balle
+from . import tank
---
+++ Tankem/src/gameLogic.py
@@ -0,0 +1,12 @@
+## -*- coding: utf-8 -*-
+import util
+
+import sys
+from panda3d.core import *
+from direct.interval.IntervalGlobal import *
+import math
+
+#Classe qui gère les phases du jeu (Menu, début, pause, fin de partie)
+class gameLogic():
+     def __init__(self):
+        mouepmoue
