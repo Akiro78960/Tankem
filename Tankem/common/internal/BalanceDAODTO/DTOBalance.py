a5f8f76f2cda476d4254ddd0d72a489c170a93cd
Talbot Sebastien <moi@sebastientalbot.com>
2017-03-01 14:15:16 -0500

    Ajout fonction getDTO() dans DTObalance.py

--- Tankem/common/internal/BalanceDAODTO/DTOBalance.py
+++ Tankem/common/internal/BalanceDAODTO/DTOBalance.py
@@ -7,23 +7,14 @@ class DTObalance():
 	def __init__(self):
             self.values = {}
 
-        # Ajouter un dictionnaire au dictionnaire
-<<<<<<< HEAD
+        # Ajouter une valeur au dictionnaire
         def appendNewValue (self, key, value):
             self.values[key] = value
-=======
 
-        def appendNewDictionary (self, key, name, value, min, max):# key: nom, name: nom détaillé
-            tmp = {}
-            tmp['name'] = name
-            tmp['value'] = value
-            tmp['min'] = min
-            tmp['max'] = max
-
-            self.dictionarInception[key] = tmp
->>>>>>> 7f9319474ed7112690f27baf201f2cc231c5df40
-
-        # Avoir un dictionnaire
+        # Avoir une valeur dans le dictionnaire
         def getValue (self, key):
             return self.values[key]
+
+        def getDTO (self):
+            return self.values
 
