692280fb6f5fe2f18cba2bd49ea0e7cd510bb5b5
Martin Nicolas <1384492@decinfo.cvm>
2017-03-01 15:16:55 -0500

    Document EmplacementDonnées créé

--- Tankem/common/internal/BalanceDAODTO/DAOBalance.py
+++ Tankem/common/internal/BalanceDAODTO/DAOBalance.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 from abc import ABCMeta, abstractmethod
 
-class DAOBalance(object):
+class DAOBalance:
 	__metaclass__ = ABCMeta
 
 	@abstractmethod
 	def __init__ (self):
 		pass
 
+	def create(self):
+		pass
+
 	def read(self):
 		pass
 
 	def update(self):
+		pass
+
+	def delete(self):
 		pass
