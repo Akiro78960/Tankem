c255833a73380c8d7ef9e0220699c24483044363
Talbot Sebastien <moi@sebastientalbot.com>
2017-03-01 17:03:46 -0500

    Fonction update du DAObalanceOracle

--- Tankem/src/common/internal/BalanceDAODTO/DAOBalanceOracle.py
+++ Tankem/src/common/internal/BalanceDAODTO/DAOBalanceOracle.py
@@ -3,28 +3,33 @@ from DAOBalance import DAOBalance
 import cx_Oracle
 
 class DAOBalanceOracle(DAOBalance):
-    __metaclass__ = DAOBalance
 
     def __init__(self):
-        self.dto = DTOBalance()
         self.connection = cx_Oracle.connect('1338283','A','10.57.4.60/DECINFO.edu')
 
     def read(self):
-        curRead = con.cursor()
+        DTO = DTObalance()
+        curRead = self.connection.cursor()
 
         keysList = curRead.execute("SELECT column_name FROM user_tab_columns WHERE table_name = 'table_nametankem_values'")
 
         curRead.close()
 
         for key in keysList:
-            curRead = con.cursor()
+            curRead = self.connection.cursor()
             value = curRead.execute("SELECT %s FROM tankem_values" % (key))
 
-            self.dto.appendNewDictionary(key, value)
+            DTO.appendNewDictionary(key, value)
 
             curRead.close()
 
-        return self.dto
+        return DTO
 
-    def update(self):
-        pass
+    def update(DTO):
+        tmpDict = DTO
+
+        curRead = self.connection.cursor()
+        for key,value in tmpDict.items():
+            curRead.execure("UPDATE tankem_values SET %s = %s" % (key,value))
+
+        curRead.close()
