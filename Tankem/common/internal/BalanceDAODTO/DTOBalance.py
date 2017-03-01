cfd5c6fa0986e4c82bd1c3af8b6d98df76569f69
Talbot Sebastien <moi@sebastientalbot.com>
2017-03-01 15:05:31 -0500

    Indentation weird DTO

--- Tankem/common/internal/BalanceDAODTO/DTOBalance.py
+++ Tankem/common/internal/BalanceDAODTO/DTOBalance.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 
-class DTObalance():
-        #####
-        # Constructeur
+class DTObalance:
+    #####
+    # Constructeur
 
-	def __init__(self):
-            self.values = {}
+    def __init__(self):
+        self.values = {}
 
-        # Ajouter une valeur au dictionnaire
-        def appendNewValue (self, key, value):
-            self.values[key] = value
+    # Ajouter une valeur au dictionnaire
+    def appendNewValue (self, key, value):
+        self.values[key] = value
 
-        # Avoir une valeur dans le dictionnaire
-        def getValue (self, key):
-            return self.values[key]
+    # Avoir une valeur dans le dictionnaire
+    def getValue (self, key):
+        return self.values[key]
 
-        def getDTO (self):
-            return self.values
-
+    def getDTO (self):
+        return self.values
