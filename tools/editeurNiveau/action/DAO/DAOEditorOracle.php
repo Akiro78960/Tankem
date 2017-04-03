62b41b11cc904617903e52ca94c32a12e57edffe
Sebastien Talbot <moi@sebastientalbot.com>
2017-04-03 12:11:20 -0400

    add empty DAO editor oracle

---
+++ tools/editeurNiveau/action/DAO/DAOEditorOracle.php
@@ -0,0 +1,22 @@
+<?php
+
+class DAOeditorOracle {
+	private static $connection;
+
+	public function __construct() {
+		$this->connection = Connection::getConnection();
+	}
+
+	public function create(){
+	}
+
+	public function read(){
+	}
+
+	public function update(){
+	}
+
+	public function delete(){
+	}
+	
+}
