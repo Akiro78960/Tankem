98bf65ffadf3ab8987c694b89cabb45de4686d45
Sebastien Talbot <moi@sebastientalbot.com>
2017-04-04 16:11:25 -0400

    Dummy Map Create

--- tools/editeurNiveau/action/DAO/DAOEditorOracle.php
+++ tools/editeurNiveau/action/DAO/DAOEditorOracle.php
@@ -7,7 +7,32 @@ class DAOeditorOracle {
 		$this->connection = Connection::getConnection();
 	}
 
-	public function create(){
+	public function create($DTOmap){
+		$statementMap = $this->connection->prepare("INSERT INTO editor_niveau (
+													name,
+													creation_date,
+													status
+													size_x
+													size_y
+													item_delay_min,
+													item_delay_max)
+													VALUES (
+													:name,
+													:creation_date,
+													:status,
+													:size_x,
+													:size_y,
+													:item_delay_min,
+													:item_delay_max)");
+
+		$statement->bindParam(':name',$DTOmap->name);
+		$statement->bindParam(':creation_date',$DTOmap->creation_date);
+		$statement->bindParam('status',$DTOmap->status);
+		$statement->bindParam('size_x',$DTOmap->size_x);
+		$statement->bindParam('size_y',$DTOmap->size_y);
+		$statement->bindParam('item_delay_min',$DTOmap->item_delay_min);
+		$statement->bindParam('item_delay_max',$DTOmap->item_delay_max);
+		$statement->execute();
 	}
 
 	public function read(){
