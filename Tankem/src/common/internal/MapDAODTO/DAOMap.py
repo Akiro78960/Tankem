c6bce8cd1f121155c32a7225f10f2eece50a23af
Talbot Sebastien <moi@sebastientalbot.com>
2017-04-05 11:15:24 -0400

    Add try/catch DTO/DAO

--- Tankem/src/common/internal/MapDAODTO/DAOMap.py
+++ Tankem/src/common/internal/MapDAODTO/DAOMap.py
@@ -11,45 +11,59 @@ class DAOmaporacle():
 
     # Connection
     def __init__(self):
+        try:
         self.connection = cx_Oracle.connect('e1384492', 'C',
                                             '10.57.4.60/DECINFO.edu')
+        except cx_Oracle.DatabaseError as e:
+            error, = e.args
+            print("Erreur de commande")
+            print(error.code)
+            print(error.message)
+            print(error.context)
 
         self.DTOlistmap = DTOlistmap()
 
     def read(self):
-        # Get array maps
-        curRead = self.connection.cursor()
-        curRead = curRead.execute("SELECT * FROM %s " % ("EDITOR_NIVEAU"))
-        arrayMapTmp = curRead.fetchall()
-        curRead.close()
-
-        # Append each map to DTOlistmap
-        for map in arrayMapTmp:
-            DTO_map_tmp = DTOmap(map[0],map[1],map[2],map[3],map[4],map[5],map[6],map[7])
-
-            # Get tuiles of map
+        try:
+            # Get array maps
             curRead = self.connection.cursor()
-            curRead.execute("SELECT * FROM EDITOR_TUILE WHERE id_niveau='%s'"
-                            % (DTO_map_tmp.getId()))
-
-            arrayTuilesTmp = curRead.fetchall()
+            curRead = curRead.execute("SELECT * FROM %s " % ("EDITOR_NIVEAU"))
+            arrayMapTmp = curRead.fetchall()
             curRead.close()
-            for tuile in arrayTuilesTmp:
-                DTO_tuile_tmp = DTOtuile(tuile[0],tuile[1],tuile[2],tuile[3],tuile[4])
-                DTO_map_tmp.appendTuile(DTO_tuile_tmp)
 
-            # Get tuiles of map
-            curRead = self.connection.cursor()
-            curRead.execute("SELECT * FROM EDITOR_SPAWN WHERE id_niveau='%s'"
-                            % (DTO_map_tmp.getId()))
+            # Append each map to DTOlistmap
+            for map in arrayMapTmp:
+                DTO_map_tmp = DTOmap(map[0],map[1],map[2],map[3],map[4],map[5],map[6],map[7])
 
-            arraySpawnTmp = curRead.fetchall()
-            curRead.close()
-            for spawn in arraySpawnTmp:
-                DTO_spawn_tmp = DTOspawn(spawn[0],spawn[1],spawn[2],spawn[3])
-                DTO_map_tmp.appendTuile(DTO_spawn_tmp)
+                # Get tuiles of map
+                curRead = self.connection.cursor()
+                curRead.execute("SELECT * FROM EDITOR_TUILE WHERE id_niveau='%s'"
+                                % (DTO_map_tmp.getId()))
 
-            # Ajouter map a la liste
-            self.DTOlistmap.appendList(DTO_map_tmp)
+                arrayTuilesTmp = curRead.fetchall()
+                curRead.close()
+                for tuile in arrayTuilesTmp:
+                    DTO_tuile_tmp = DTOtuile(tuile[0],tuile[1],tuile[2],tuile[3],tuile[4])
+                    DTO_map_tmp.appendTuile(DTO_tuile_tmp)
+
+                # Get tuiles of map
+                curRead = self.connection.cursor()
+                curRead.execute("SELECT * FROM EDITOR_SPAWN WHERE id_niveau='%s'"
+                                % (DTO_map_tmp.getId()))
+
+                arraySpawnTmp = curRead.fetchall()
+                curRead.close()
+                for spawn in arraySpawnTmp:
+                    DTO_spawn_tmp = DTOspawn(spawn[0],spawn[1],spawn[2],spawn[3])
+                    DTO_map_tmp.appendTuile(DTO_spawn_tmp)
+
+                # Ajouter map a la liste
+                self.DTOlistmap.appendList(DTO_map_tmp)
+            except cx_Oracle.DatabaseError as e:
+                error, = e.args
+                print("Erreur de commande")
+                print(error.code)
+                print(error.message)
+                print(error.context)
 
         return self.DTOlistmap
