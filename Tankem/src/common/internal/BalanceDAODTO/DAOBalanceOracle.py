c090d16de6f6d58589f5070b8491516426059051
Talbot Sebastien <moi@sebastientalbot.com>
2017-04-05 18:27:00 -0400

    Revert "Add try/catch DTO/DAO"
    This reverts commit c6bce8cd1f121155c32a7225f10f2eece50a23af.

--- Tankem/src/common/internal/BalanceDAODTO/DAOBalanceOracle.py
+++ Tankem/src/common/internal/BalanceDAODTO/DAOBalanceOracle.py
@@ -8,60 +8,41 @@ class DAOBalanceOracle(DAOBalance):
     def __init__(self):
         try:
             self.connection = cx_Oracle.connect('e1384492','C','10.57.4.60/DECINFO.edu')
-        except cx_Oracle.DatabaseError as e:
-            error, = e.args
-            print("Erreur de commande")
-            print(error.code)
-            print(error.message)
-            print(error.context)
+        except Exception as e:
+            raise e
 
     def read(self, table_name):
         table_name = table_name.upper()
         tmpDTO = DTObalance()
-        try:
-            curRead = self.connection.cursor()
+        curRead = self.connection.cursor()
 
-            curRead = curRead.execute("SELECT column_name FROM user_tab_columns WHERE table_name = '%s'" % (table_name))
-            keysList = curRead.fetchall()
+        curRead = curRead.execute("SELECT column_name FROM user_tab_columns WHERE table_name = '%s'" % (table_name))
+        keysList = curRead.fetchall()
+
+        curRead.close()
+
+        for key in keysList:
+            curRead = self.connection.cursor()
+            curRead.execute("SELECT %s FROM %s" % (key[0],table_name))
+            value = curRead.fetchall()
+
+            tmpDTO.setValue(key[0], value[0][0])
 
             curRead.close()
 
-            for key in keysList:
-                curRead = self.connection.cursor()
-                curRead.execute("SELECT %s FROM %s" % (key[0],table_name))
-                value = curRead.fetchall()
-
-                tmpDTO.setValue(key[0], value[0][0])
-
-                curRead.close()
-
-            tmpDTO.setValue("TABLE_NAME", table_name)
-        except cx_Oracle.DatabaseError as e:
-            error, = e.args
-            print("Erreur de commande")
-            print(error.code)
-            print(error.message)
-            print(error.context)
+        tmpDTO.setValue("TABLE_NAME", table_name)
 
         return tmpDTO
 
     def update(self, DTO):
-        try:
-            tmpDict = DTO.getDictionary()
-            tmpID = DTO.getValue("ID")
-            table_name = DTO.getValue("TABLE_NAME")
+        tmpDict = DTO.getDictionary()
+        tmpID = DTO.getValue("ID")
+        table_name = DTO.getValue("TABLE_NAME")
 
-            for key,value in tmpDict.items():
-                if(key != "ID" and key != "TABLE_NAME"):
-                    curRead = self.connection.cursor()
-                    curRead.execute("UPDATE %s SET %s = %s WHERE id = %s" % (table_name,key,value,tmpID))
-                    curRead.close()
-            self.connection.commit()
-        except cx_Oracle.DatabaseError as e:
-            error, = e.args
-            print("Erreur de commande")
-            print(error.code)
-            print(error.message)
-            print(error.context)
-
+        for key,value in tmpDict.items():
+            if(key != "ID" and key != "TABLE_NAME"):
+                curRead = self.connection.cursor()
+                curRead.execute("UPDATE %s SET %s = %s WHERE id = %s" % (table_name,key,value,tmpID))
+                curRead.close()
+        self.connection.commit()
 
