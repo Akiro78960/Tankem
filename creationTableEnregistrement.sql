406004e7c2fd040e5c0875e5c8f23461c88daaf0
Sebastien Talbot <moi@sebastientalbot.com>
2017-04-13 15:03:31 -0400

    Dummy Script sql enregistrement

---
+++ creationTableEnregistrement.sql
@@ -0,0 +1,39 @@
+DROP TABLE editor_spawn;
+
+CREATE TABLE enregistrement_partie (
+	id NUMBER GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+	creation_date VARCHAR2(10) NOT NULL
+);
+
+CREATE TABLE enregistrement_joueur (
+	no_joueur NUMBER NOT NULL,
+	id_partie NUMBER NOT NULL,
+	time_sec NUMBER NOT NULL,
+	pos_x NUMBER NOT NULL,
+	pos_y NUMBER NOT NULL,
+	CONSTRAINT pk_erg_joueur PRIMARY KEY (no_joueur, id_partie, time_sec),
+	CONSTRAINT fk_erg_partie_joueur FOREIGN KEY (id_partie) REFERENCES enregistrement_partie(id)
+);
+
+CREATE TABLE enregistrement_arme (
+	type_arme NUMBER NOT NULL,
+	id_partie NUMBER NOT NULL,
+	time_sec NUMBER NOT NULL,
+	pos_x NUMBER NOT NULL,
+	pos_y NUMBER NOT NULL,
+	CONSTRAINT pk_erg_arme PRIMARY KEY (id_partie, time_sec, pos_x, pos_y),
+	CONSTRAINT fk_erg_partie_arme FOREIGN KEY (id_partie) REFERENCES enregistrement_partie(id)
+);
+
+-- A MODIFIER PEUT-ETRE
+CREATE TABLE enregistrement_projectile (
+	id_partie NUMBER NOT NULL,
+	time_sec NUMBER NOT NULL,
+	pos_x NUMBER NOT NULL,
+	pos_y NUMBER NOT NULL,
+	pos_z NUMBER NOT NULL,
+	en_mouvement NUMBER(1) NOT NULL,
+	CONSTRAINT pk_erg_projectile PRIMARY KEY (id_partie, time_sec, pos_x, pos_y, pos_z),
+	CONSTRAINT fk_erg_partie_projectile FOREIGN KEY (id_partie) REFERENCES enregistrement_partie(id)
+
+);
