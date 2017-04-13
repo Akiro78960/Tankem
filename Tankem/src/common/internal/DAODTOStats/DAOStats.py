ddce396220c33a5e8d8aa56deffbbee6e16f0222
Clovis <clo.perin@gmail.com>
2017-04-13 15:14:37 -0400

    ajout debut DAO

--- Tankem/src/common/internal/DAODTOStats/DAOStats.py
+++ Tankem/src/common/internal/DAODTOStats/DAOStats.py
@@ -1,19 +1,11 @@
 from DTOStats import DTOStats
+from SingletonDBConnection import SingletonDBConnection
 
 
 class DAOStats():
 
     def __init__(self):
-		try:
-			self.connection = cx_Oracle.connect('e1384492', 'C',
-											'10.57.4.60/DECINFO.edu')
-		except cx_Oracle.DatabaseError as e:
-			error, = e.args
-			print("Erreur de commande")
-			print(error.code)
-			print(error.message)
-			print(error.context)
-
+		self.connection = SingletonDBConnection().getConnection()
 		self.DTOStats= DTOStats()
 
 
