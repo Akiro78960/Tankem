2346dd34b3fe37ef0a0b9416898bc5207ae44cd2
Clovis <clo.perin@gmail.com>
2017-05-03 17:34:19 -0400

    ajout affichage map prefere

--- action/AjaxNiveauFavori.php
+++ action/AjaxNiveauFavori.php
@@ -14,10 +14,19 @@
 				$this->connection = new PDO("oci:dbname=DECINFO", "e1384492", "C");
                 $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
                 $this->connection->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
-				$statement = $this->connection->prepare("SELECT COUNT(idNiveau) as Count, idNiveau FROM partie WHERE IdJoueur1 = ? or IdJoueur2 = ? GROUP BY idNiveau ORDER by Count DESC");
+				$statement = $this->connection->prepare("SELECT COUNT(idNiveau) Count, idNiveau FROM partie WHERE IdJoueur1 = ? or IdJoueur2 = ? GROUP BY idNiveau ORDER by Count DESC");
 				$statement->execute(Array($id, $id));
 				$this->row = $statement->fetchall(PDO::FETCH_ASSOC);
-				$this->result = json_encode($this->row);
+				// $this->result = json_encode($this->row);
+				// if($this->row == null){
+				// 	$this->result = null;
+				// }else{
+					$this->idMostUsed = json_encode($this->row);
+					// $this->idMostUsed = $this->row;
+					$this->result = $this->idMostUsed;
+					// print_r($this->result)
+
+				// }
 			}catch(PDOException $e){
 				echo 'Échec lors de la connection : ' + $e->getMessage();
 			}
--- js/search.js
+++ js/search.js
@@ -1,5 +1,10 @@
+var resultSearch = null
+var mapFavorites = []
+
 // $(document).ready(function() {
-//     console.log("ready");
+//     console.log("ready")
+//     var resultSearch = null
+//     var mapFavorites = []
 // });
 
 function search(){
@@ -11,72 +16,115 @@ function search(){
     })
     .done(function(r) {
         document.getElementById("searchResults").innerHTML=""
-        result = JSON.parse(r)
-        console.log(result);
-        $(result).each(function(index) {
-            /////////////AJAX PRINCIPAL///////////
-            node = document.createElement("div")
-            node.className = "container"
-            node.style="background-color:F0F0F0; padding:1%; margin:1% auto;"
-            row1 = document.createElement("div")
-            row1.className="row"
-            row2 = document.createElement("div")
-            row2.className="row"
-            row3 = document.createElement("div")
-            row3.className="row"
+        resultSearch = JSON.parse(r)
+        console.log(resultSearch);
+        getFavoritMap()
 
-            //row1
-            colName = document.createElement("div")
-            colName.className="col-sm-12"
-            txtname = document.createElement("h4")
-            txtname.className="text-center"
-            texte = document.createTextNode(""+result[index].USERNAME + " " + calculateName(result[index]) + " ("+result[index].SURNAME+" "+result[index].NAME+")")
-            txtname.appendChild(texte)
-            colName.appendChild(txtname)
-            row1.appendChild(colName)
-            row1.style="background-color:E0E0E0;border:1px solid black"
-
-
-            //row2
-            colImg = document.createElement("div")
-            img = document.createElement("img")
-            colImg.className="col-sm-2"
-            img.src="images/tankAlpha.png"
-            img.style="width:150px; height:150px; background-color:"+result[index].COULEURTANK
-            colImg.appendChild(img)
-
-            col1Stats = document.createElement("div")
-            col1Stats.className="col-sm-5"
-            col1Stats.style="font-weight:bold;font-size:17px;line-height:35px;padding-top:5px;border-left: 1px solid black; border-right: 1px solid black; padding-left:3%; margin-left:3%;"
-            col1Stats.appendChild(document.createTextNode("Niveau: "+result[index].NIVEAU))
-            col1Stats.appendChild(document.createElement("br"))
-            col1Stats.appendChild(document.createTextNode("Parties gagnees: "+result[index].PARTIEGAGNE))
-            col1Stats.appendChild(document.createElement("br"))
-            col1Stats.appendChild(document.createTextNode("Parties jouees: "+result[index].PARTIEJOUE))
-            col1Stats.appendChild(document.createElement("br"))
-            if(result[index].PARTIEGAGNE == 0){
-                col1Stats.appendChild(document.createTextNode("Ratio victoire: 0%"))
-            }else{
-                col1Stats.appendChild(document.createTextNode("Ratio victoire: "+(result[index].PARTIEJOUE/result[index].PARTIEGAGNE)*100+"%"))
-            }
-
-            row2.appendChild(colImg)
-            row2.appendChild(col1Stats)
-
-
-            //row3
-            
-
-
-            node.appendChild(row1)
-            node.appendChild(row2)
-            node.appendChild(row3)
-            // node.appendChild(document.createElement("br"))
-            // node.appendChild(document.createTextNode("Niveau: "+result[index].NIVEAU))
-            // node.appendChild(document.createElement("br"))
-            document.getElementById("searchResults").appendChild(node)
-
-        });
     })
+}
 
+function getFavoritMap(){
+    $(resultSearch).each(function(index, el) {
+        $.ajax({
+            type : "POST",
+            url : "ajaxNiveauFavori.php",
+            data : {
+                idJoueur: resultSearch[index].ID
+            }
+        })
+        .done(function(favouritmap) {
+            mapFavorites[index] = JSON.parse(favouritmap)
+            getNameFavMap(index)
+        })
+    })
+}
+
+function getNameFavMap(index){
+        if(mapFavorites[index].length == 0){
+            mapFavorites[index] = "NONE"
+        }else{
+            $.ajax({
+                type : "POST",
+                url : "ajaxGetNomMap.php",
+                data : {
+                    idNiveau: mapFavorites[index][0].IDNIVEAU
+                }
+            })
+            .done(function(r) {
+                console.log("success");
+                reponse = JSON.parse(r)
+                mapFavorites[index] = reponse[0].NAME
+                console.log(mapFavorites[index]);
+
+                    displayInfos()
+            })
+        }
+}
+
+function displayInfos(){
+    document.getElementById("searchResults").innerHTML=""
+    $(resultSearch).each(function(index) {
+        /////////////AJAX PRINCIPAL///////////
+        node = document.createElement("div")
+        node.className = "container"
+        node.style="background-color:F0F0F0; padding:1%; margin:1% auto;"
+        row1 = document.createElement("div")
+        row1.className="row"
+        row2 = document.createElement("div")
+        row2.className="row"
+        row3 = document.createElement("div")
+        row3.className="row"
+
+        //row1
+        colName = document.createElement("div")
+        colName.className="col-sm-12"
+        txtname = document.createElement("h4")
+        txtname.className="text-center"
+        texte = document.createTextNode(""+resultSearch[index].USERNAME + " " + calculateName(resultSearch[index]) + " ("+resultSearch[index].SURNAME+" "+resultSearch[index].NAME+")")
+        txtname.appendChild(texte)
+        colName.appendChild(txtname)
+        row1.appendChild(colName)
+        row1.style="background-color:E0E0E0;border:1px solid black"
+
+
+        //row2
+        colImg = document.createElement("div")
+        img = document.createElement("img")
+        colImg.className="col-sm-2"
+        img.src="images/tankAlpha.png"
+        img.style="width:150px; height:150px; background-color:"+resultSearch[index].COULEURTANK
+        colImg.appendChild(img)
+
+        //col1 Stats
+        col1Stats = document.createElement("div")
+        col1Stats.className="col-sm-5"
+        col1Stats.style="font-weight:bold;font-size:17px;line-height:35px;padding-top:5px;border-left: 1px solid black; border-right: 1px solid black; padding-left:3%; margin-left:3%;"
+        col1Stats.appendChild(document.createTextNode("Niveau: "+resultSearch[index].NIVEAU))
+        col1Stats.appendChild(document.createElement("br"))
+        col1Stats.appendChild(document.createTextNode("Parties gagnees: "+resultSearch[index].PARTIEGAGNE))
+        col1Stats.appendChild(document.createElement("br"))
+        col1Stats.appendChild(document.createTextNode("Parties jouees: "+resultSearch[index].PARTIEJOUE))
+        col1Stats.appendChild(document.createElement("br"))
+        if(resultSearch[index].PARTIEGAGNE == 0){
+            col1Stats.appendChild(document.createTextNode("Ratio victoire: 0%"))
+        }else{
+            col1Stats.appendChild(document.createTextNode("Ratio victoire: "+(resultSearch[index].PARTIEJOUE/resultSearch[index].PARTIEGAGNE)*100+"%"))
+        }
+
+        col2Stats = document.createElement("div")
+        col2Stats.className="col-sm-4"
+        col2Stats.style="font-weight:bold;font-size:17px;line-height:35px;padding-top:5px;"
+
+        col2Stats.appendChild(document.createTextNode("Map favorite: "+mapFavorites[index]))
+
+        row2.appendChild(colImg)
+        row2.appendChild(col1Stats)
+        row2.appendChild(col2Stats)
+
+        node.appendChild(row1)
+        node.appendChild(row2)
+        node.appendChild(row3)
+        document.getElementById("searchResults").appendChild(node)
+
+    });
 }
